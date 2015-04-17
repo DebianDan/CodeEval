e83c041c44cc21dff50a7d735bd8d5817580dc21
Dan Tucholski <dantucholski@gmail.com>
2015-04-17 01:54:34 -0700

    Optimized the Sum of Ints to use Kadane's algo for find the max subarray in linear time.  2 revisions the latter not allowing for zero-length subarrays, therefore a negative max could be returned

--- Moderate/Sum_Of_Integers/python/sum_of_ints.py
+++ Moderate/Sum_Of_Integers/python/sum_of_ints.py
@@ -1,16 +1,42 @@
 import sys
 
+def sliding_window_brute_force(nums):
+	max_sum = nums[0]
+	for i in xrange(1, len(nums)+1):
+		for j in xrange(len(nums)+1-i):
+			sliding_sum = sum(nums[j:j+i])
+			if sliding_sum > max_sum:
+				max_sum = sliding_sum
+	return max_sum
+
+
+# Implementing Traditional Kadane's algorithm to the Maximum subarray problem
+# http://en.wikipedia.org/wiki/Maximum_subarray_problem
+# Runs in O(n) linear time
+def traditional_max_subarray(nums):
+	max_ending_here = current_max = 0
+	for num in nums:
+	    max_ending_here = max(0, max_ending_here + num)
+	    current_max = max(current_max, max_ending_here)
+	return current_max
+
+# Implementing Kadane's algorithm to the Maximum subarray problem
+# http://en.wikipedia.org/wiki/Maximum_subarray_problem
+# Runs in O(n) linear time
+# The max could be negative, instead of allowing zero-len subarrays 
+def max_subarray(nums):
+	max_ending_here = current_max = nums[0]
+	for num in nums[1:]:
+	    max_ending_here = max(num, max_ending_here + num)
+	    current_max = max(current_max, max_ending_here)
+	return current_max
+
 def main(filename):
 	with open(filename, 'r') as f:
 		for line in f:
 			nums = map(int, line.strip().split(","))
-			max_sum = nums[0]
-			for i in xrange(1, len(nums)+1):
-				for j in xrange(len(nums)+1-i):
-					sliding_sum = sum(nums[j:j+i])
-					if sliding_sum > max_sum:
-						max_sum = sliding_sum
-			print max_sum
+			print max_subarray(nums)
+
 
 if len(sys.argv) != 2:
 	print "Usage ./max_range_sum.py <filename>"
